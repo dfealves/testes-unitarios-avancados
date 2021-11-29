75055c9c3a91bce53318e31d0d79b6f4cb19b3f2
dfealves <daniloferreira.alves@outlook.com>
2021-11-29 07:35:20 -0300

    feature: add spyOn and test UI

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
+import { PhotoFrameComponent } from './shared/components/photo-frame/photo-frame.component';
+
+@NgModule({
+  declarations: [AppComponent, PhotoFrameComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LikeWidgetModule,
+    PhotoFrameModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/components/photo-frame/photo-frame.module.ts
@@ -0,0 +1,14 @@
+import { LikeWidgetModule } from './../like-widget/like-widget.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LikeWidgetModule
+  ]
+})
+export class PhotoFrameModule { }
